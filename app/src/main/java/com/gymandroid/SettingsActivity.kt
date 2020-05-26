ed52df0559b36cc34ad83118dc1cfbc76ce36c9e
haoweilou <591193117@qq.com>
2020-05-27 03:13:57 +1000

    ''

--- app/src/main/java/com/gymandroid/SettingsActivity.kt
+++ app/src/main/java/com/gymandroid/SettingsActivity.kt
@@ -3,11 +3,13 @@ package com.gymandroid
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_setting.*
 
 @SuppressLint("Registered")
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
@@ -15,5 +17,6 @@ class SettingsActivity : AppCompatActivity() {
             finish()
             overridePendingTransition(R.anim.exit_old_from_right, R.anim.exit_new_from_right)
         }
+        val EditText: weight = findViewById(R.id.weight)
     }
 }
