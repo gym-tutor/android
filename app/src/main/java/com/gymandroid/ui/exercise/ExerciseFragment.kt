6012343391995124d56c12b616f5f3c81767a266
Haodong Lu <lhd32262175@gmail.com>
2020-05-27 13:30:21 +1000

    excercise page init v1.1

--- app/src/main/java/com/gymandroid/ui/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/gymandroid/ui/exercise/ExerciseFragment.kt
@@ -6,10 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.gymandroid.R
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_exercise.*
+
 
 class ExerciseFragment : Fragment() {
 
@@ -35,9 +42,22 @@ class ExerciseFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        val navHostFragment = nav_host_fragment as NavHostFragment
+//        val params = navHostFragment.layoutInflater as ConstraintLayout.LayoutParams
+//        params.verticalBias = 0.5f
+        // val navHostFragment = navFragment as NavHostFragment
+        // val host_view : NavHostFragment = findFragmentById(R.id.nav_host_fragment)
         val root = inflater.inflate(R.layout.fragment_exercise, container, false)
-        startActivity(Intent(activity, excerciseListActivity::class.java))
+//        button.setOnClickListener() {
+//            startActivity(Intent(this.activity, excerciseListActivity::class.java))
+//        }
+        // startActivity(Intent(activity, excerciseListActivity::class.java))
         return root
     }
 
+//    public fun excercise_start() {
+//        startActivity(Intent(this.activity, excerciseListActivity::class.java))
+//    }
+//
+//    button.
 }
