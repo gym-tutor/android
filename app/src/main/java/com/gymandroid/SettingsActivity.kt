46d5aa6af1444e72f33826987a59ca91f5babd89
haoweilou <591193117@qq.com>
2020-05-27 20:40:11 +1000

    'bmi'

--- app/src/main/java/com/gymandroid/SettingsActivity.kt
+++ app/src/main/java/com/gymandroid/SettingsActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_setting.*
 
@@ -17,6 +19,19 @@ class SettingsActivity : AppCompatActivity() {
             finish()
             overridePendingTransition(R.anim.exit_old_from_right, R.anim.exit_new_from_right)
         }
-        val EditText: weight = findViewById(R.id.weight)
+        val bmi: Button = findViewById(R.id.calculate_bmi) as Button
+        bmi.setOnClickListener{
+            val height: EditText = findViewById(R.id.height) as EditText
+            val heightvalue:Double = height.text.toString().toDouble()
+            val weight: EditText = findViewById(R.id.weight) as EditText
+            val weightvalue:Double = weight.text.toString().toDouble()
+            val bmivalue: Double = weightvalue/(heightvalue*heightvalue/10000)
+            val bmi_output: TextView = findViewById(R.id.bmi) as TextView
+            bmi_output.text ="%.2f".format(bmivalue)
+        }
+        val changecolor: Switch = findViewById(R.id.change_theme) as Switch
+
+
+
     }
 }
