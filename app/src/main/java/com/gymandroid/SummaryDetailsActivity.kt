19f030a80b1743cea4408eadedd8ee5e02de27e8
Bofei Wang <secure@bofeiwang.com>
2020-06-09 02:39:21 +1000

    implement hard coded summary details page

--- app/src/main/java/com/gymandroid/SummaryDetailsActivity.kt
+++ app/src/main/java/com/gymandroid/SummaryDetailsActivity.kt
@@ -1,22 +1,15 @@
 package com.gymandroid
 
-import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.AttributeSet
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.gymandroid.ui.summary.RecordCardAdapter
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
 import kotlinx.android.synthetic.main.activity_summary_details.*
+
 
 class SummaryDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,5 +19,34 @@ class SummaryDetailsActivity : AppCompatActivity() {
             finish()
             overridePendingTransition(R.anim.exit_old_from_right, R.anim.exit_new_from_right)
         }
+
+        inflateCorrectRateChart(85f)
+    }
+
+    fun inflateCorrectRateChart(correctRate: Float) {
+        val pieChart = findViewById<PieChart>(R.id.piechart)
+        val correctRateData = ArrayList<PieEntry>()
+
+        correctRateData.add(PieEntry(85f))
+        correctRateData.add(PieEntry(15f))
+        val dataSet = PieDataSet(correctRateData, null)
+
+        pieChart.data = PieData(dataSet)
+        dataSet.setColors(
+            // green and red
+            Color.rgb(0x3d, 0xdc, 0x84),
+            Color.rgb(0xba, 0x16, 0x0c)
+        )
+
+        pieChart.centerText = "85% Correct"
+        pieChart.setTransparentCircleAlpha(1)
+        pieChart.setCenterTextSize(24f)
+
+        dataSet.setDrawValues(false)
+        dataSet.valueFormatter = PercentFormatter(pieChart)
+        dataSet.valueTextSize = 18f
+        dataSet.valueTextColor = Color.WHITE
+        pieChart.description.isEnabled = false
+        pieChart.legend.isEnabled = false
     }
 }
