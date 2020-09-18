5c755f82871afb0b397c021507c2058b213fd352
Haodong Lu <lhd32262175@gmail.com>
2020-09-18 18:21:13 +1000

    JSON helper init

---
+++ app/src/main/java/com/gymandroid/JsonHelper/JsonWrite.py
@@ -0,0 +1,11 @@
+
+import json
+
+person_dict = {"name": "Bob",
+"languages": ["English", "Fench"],
+"married": True,
+"age": 32
+}
+
+with open('person.txt', 'w') as json_file:
+  json.dump(person_dict, json_file)
